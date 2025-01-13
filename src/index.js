33bfb8eff88bfed552d1b5ccd0d2b82412cf2e12
Servacek <filip.mitka007@gmail.com>
2025-01-13 20:00:53 +0100

    Added option to configure IDs.

--- src/index.js
+++ src/index.js
@@ -138,7 +138,10 @@ function onChunkReceived(chunk) {
             bitsReceivedStr = "";
 
             if (receivedString && receivedString.trim().length > 0) {
-                displayMessageAtBottom(createUserMessage("DROID", CONST.ALIGMENT_LEFT, receivedString.trim()))
+                const authorId = buffer[1];
+                // TODO: Add option to assign names to IDs in the config tab.
+                const authorName = authorId.toString()
+                displayMessageAtBottom(createUserMessage(authorName, CONST.ALIGMENT_LEFT, receivedString.trim()))
             }
 
 
@@ -233,7 +236,7 @@ async function tryStartRecording() {
 
 
             // 5500
-            const SAMPLE_CHUNK_SIZE = 5200;// WASM.MEMORY_U32[WASM.EXPORTS.SAMPLE_CHUNK_SIZE/4];
+            const SAMPLE_CHUNK_SIZE = 5700;// WASM.MEMORY_U32[WASM.EXPORTS.SAMPLE_CHUNK_SIZE/4];
             const BITS_PER_FRAME = WASM.MEMORY_U32[WASM.EXPORTS.BITS_PER_FRAME/4];
 
             if (chunkBuffer.length < SAMPLE_CHUNK_SIZE) {
--- src/tabs/config.js
+++ src/tabs/config.js
@@ -4,6 +4,8 @@ import {MEMORY, EXPORTS} from "../bindings.js";
 const bitsPerFrameSlider = document.getElementById("bits-per-frame");
 const bitsPerFrameDisplay = document.getElementById("bits-per-frame-display");
 
+const channelIdSelect = document.getElementById("channel-id-select");
+
 
 function onConfigurationUpdated() {
     // We always have to recalculate the dynamic configurations.
@@ -20,3 +22,22 @@ bitsPerFrameSlider.addEventListener("input", (event) => {
 
     onConfigurationUpdated();
 });
+
+window.addEventListener("wasm-library-loaded", () => {
+    const MAX_USERS = MEMORY[EXPORTS.MAX_USERS.value];
+    for (let i = 0; i < MAX_USERS; i++) {
+        const option = document.createElement("option");
+        option.value = i;
+        option.textContent = "Kanál #" + i;
+        channelIdSelect.appendChild(option);
+    }
+
+    // Select the default user ID
+    const CURRENT_USER_ID = MEMORY[EXPORTS.USER_ID.value];
+    channelIdSelect.value = CURRENT_USER_ID;
+
+    channelIdSelect.addEventListener("change", () => {
+        MEMORY[EXPORTS.USER_ID.value] = new Uint8Array([channelIdSelect.value])[0];
+        onConfigurationUpdated();
+    });
+})
