82663b011d30dc437b5206202371883df8b0f16e
Servacek <105163129+Servacek@users.noreply.github.com>
2025-02-19 09:51:47 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -142,7 +142,7 @@ function onChunkReceived(chunk) {
             if (receivedString && receivedString.trim().length > 0) {
                 const authorId = buffer[1];
                 const MAX_USERS = WASM.MEMORY[WASM.EXPORTS.MAX_USERS.value];
-                if (!authorId || authorId >= MAX_USERS) {
+                if (autohorId < 0 || authorId >= MAX_USERS) {
                     console.log("Invalid author ID:", authorId);
                     return;
                 }
